dbde945f680844893f4fe975164ed9ed7ae8dfc0
codem <altaf.badure.it@ghrcem.raisoni.net>
2025-01-18 06:16:12 +0530

    msbbodbioiidbb

---
+++ day2/day2.py
@@ -0,0 +1,12 @@
+def nStarTriangle(n :int):
+    for i in range(n):
+        for j in range(n-i-1):
+            print(" ", end="")
+        for j in range(2*i+1):
+            print("*",end="")
+        for j in range(n-i-1):
+            print(" ", end="")
+        print()
+
+
+nStarTriangle(5)
