12543a63316a478c0d3f87a63ccfd4c18c6cce47
codem <altaf.badure.it@ghrcem.raisoni.net>
2025-01-27 06:08:46 +0530

    Update day2.py

--- day2/day2.py
+++ day2/day2.py
@@ -19,6 +19,16 @@
 #
 # print(head["Next"]["Next"]["Next"]["Next"]["value"])
 
+def nStarTriangle(n :int):
+    for i in range(n):
+        for j in range(n-i-1):
+            print(" ", end="")
+        for j in range(2*i+1):
+            print("*",end="")
+        for j in range(n-i-1):
+            print(" ", end="")
+        print()
+nStarTriangle(5)
 
 class Node:
      def __init__(self, value):
